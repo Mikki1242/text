5fe85c948041db0acdc90abdf452771626bdb51d
Mikki1242 <77524885+Mikki1242@users.noreply.github.com>
2021-01-24 17:39:19 +0700

    Add files via upload

---
+++ JS/Script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+      minSlides: 1, // Это минимальное количество слайдов
+      maxSlides: 3, // Это максимальное количество слайдов
+      slideWidth: 258 // Ширина одного слайда
+    });
+  });
